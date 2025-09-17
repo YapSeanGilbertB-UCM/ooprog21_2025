6f8f00efac63793d30906e68b5e5c9554c18c0bb
YapSeanGilbertB-UCM <seanyap39@gmail.com>
2025-09-17 16:40:39 +0800

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,15 @@
+
+
+public class Comments {
+    public static void main(String[] args) {
+        
+        
+        System.out.println("This program demonstrates comments in Java.");
+        
+     
+        System.out.println("This is another line of code.");
+        
+      
+        System.out.println("This is the third line of code.");
+    }
+}
---
+++ FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Cause I don't think that they'd understand");
+        System.out.println("But I'm a creep");
+        System.out.println("Hihintayin kita, kahit nasa'n ka pa");
+        System.out.println("Binaon naman na ang lahat");
+    }
+}

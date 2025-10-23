46e99c0c802d336aec63ac0d22b67cdd6bf3b1f1
YapSeanGilbertB-UCM <seanyap39@gmail.com>
2025-10-23 20:47:32 +0800

    Add files via upload

--- Chapter_6/BankBalance.java
+++ Chapter_6/BankBalance.java
@@ -3,24 +3,29 @@ import java.util.Scanner;
 public class BankBalance {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double balance;
-        final double INTEREST_RATE = 0.03; 
+
+        double balance, interestRate = 0.03;
         int year = 1;
         int choice;
 
-        System.out.print("Enter initial bank balance > ");
+        System.out.print("Enter initial balance: $");
         balance = input.nextDouble();
 
-        System.out.print("Do you want to see next year's balance?\nEnter 1 for yes or any other number for no >> ");
-        choice = input.nextInt();
+        do {
+            balance += balance * interestRate;
 
-        while (choice == 1) {
-            balance = balance + (balance * INTEREST_RATE);
-            System.out.printf("After year %d at %.2f interest rate, balance is $%.1f%n", year, INTEREST_RATE, balance);
+            System.out.printf("After year %d at %.2f interest rate, balance is $%.4f%n",
+                              year, interestRate, balance);
+
+            System.out.println("Do you want to see the balance at the end of another year?");
+            System.out.print("Enter 1 for yes\n or any other number for no >> ");
+            choice = input.nextInt();
+
             year++;
 
-            System.out.print("\nDo you want to see the balance at the end of another year?\nEnter 1 for yes or any other number for no >> ");
-            choice = input.nextInt();
-        }
+        } while (choice == 1);
+
+        System.out.println("----jGRASP: Operation complete.");
+        input.close();
     }
 }
